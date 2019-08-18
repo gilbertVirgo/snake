0508cce80bb4c0243c862a563ee5c959e093bae6
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-08-18 21:51:16 +0200

    Undo that

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -3,4 +3,4 @@ export const API_ROOT = (
     window.location.hostname === "127.0.0.1" || 
     window.location.hostname === "") ? 
     "http://localhost:4013" : 
-    "http://172.31.44.74"; // local ip to prevent websocket errors
+    "http://172.31.44.74:4013"; // local ip to prevent websocket errors
