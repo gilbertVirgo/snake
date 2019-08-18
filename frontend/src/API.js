931506a5429124b5a773aae7155d0c7bea7c5e39
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-08-18 13:33:06 +0200

    Added API root

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,6 +1,11 @@
 import request from "request";
 
-const API_ROOT = "http://localhost:8000";
+const API_ROOT = (
+    location.hostname === "localhost" || 
+    location.hostname === "127.0.0.1" || 
+    location.hostname === "") ? 
+    "http://localhost:8000" : 
+    "http://snake-api.gilbertvirgo.com";
 
 export const Room = {
     create: async roomName => 
