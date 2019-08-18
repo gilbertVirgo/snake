3aff5eece2c8c2cfc175435a34987e952fcbc7ad
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-08-19 00:00:24 +0200

    localhost attempt

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,6 +1,8 @@
-export const API_ROOT = (
-    window.location.hostname === "localhost" || 
-    window.location.hostname === "127.0.0.1" || 
-    window.location.hostname === "") ? 
-    "http://localhost:4013" : 
-    "http://172.31.44.74:4013"; // local ip to prevent websocket errors
+// export const API_ROOT = (
+//     window.location.hostname === "localhost" || 
+//     window.location.hostname === "127.0.0.1" || 
+//     window.location.hostname === "") ? 
+//     "http://localhost:4013" : 
+//     "http://172.31.44.74:4013"; // local ip to prevent websocket errors
+
+export const API_ROOT = "http://localhost:4013";
