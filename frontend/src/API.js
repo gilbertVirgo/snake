63ea167f9fb20e7b82295389d03c74ee80e55cd0
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-08-18 21:34:48 +0200

    Added API root constant

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,17 +1,6 @@
-import request from "request";
-
 export const API_ROOT = (
     location.hostname === "localhost" || 
     location.hostname === "127.0.0.1" || 
     location.hostname === "") ? 
     "http://localhost:4013" : 
     "http://snake-api.gilbertvirgo.com";
-
-export const Room = {
-    create: async roomName => 
-        await request.put(API_ROOT + "/room", {name: roomName}),
-    join: id => 
-        await request.post(API_ROOT + "/room", {id}),
-    getAll: () => 
-        await request.get(API_ROOT + "/rooms")
-}
